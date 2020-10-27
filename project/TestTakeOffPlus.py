fix: beta plot & alpha calculation

project/TestTakeOffPlus.py
@@ -1071,18 +1071,23 @@ class TestTakeOffPlus(unittest.TestCase):
         log_single_state(2, 'body_theta')
         log_single_state(3, 'alpha')
 
+        print('TESTOINGASDFASDFASDF')
+        test = np.add(calculated_state_log[2, :], calculated_state_log[3, :])
+        print(test[2000])
+        print('max: ' + str(np.amin(test)))
+
         plt.figure().set_size_inches(10, 5)
         plt.plot(simulated_sample_times, np.add(
             simulated_state_log[2, :], simulated_state_log[3, :]))
         plt.plot(simulated_sample_times, np.add(
             simulated_state_log[2+5, :], simulated_state_log[3+5, :]))
         plt.plot(simulated_sample_times, np.add(
-            calculated_state_log[0+5, :], calculated_state_log[0+5, :]))
+            calculated_state_log[2, :], calculated_state_log[3, :]))
         plt.plot(simulated_sample_times, np.add(
             calculated_state_log[2+5, :], calculated_state_log[3+5, :]))
+        plt.grid(True)
         plt.legend(["simulated_beta", "simulated_beta_d",
                     "calculated_beta", "calculated_beta_d"])
-        plt.grid(True)
 
         log_single_state(4, 'leg_extension')
 