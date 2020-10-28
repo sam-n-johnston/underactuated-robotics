fix: change sign for speed

project/TestTakeOffPlus.py
@@ -825,10 +825,11 @@ class TestTakeOffPlus(unittest.TestCase):
 
     #     self.liftoff_minus_state(apex_state)
 
+    # GOOD
     def test_liftoff_minus_state_1(self):
         apex_state = np.zeros(10)
         apex_state[1] = 3.5  # height
-        apex_state[2] = -0.15  # theta
+        apex_state[2] = -0.15  # alpha
         apex_state[4] = 0.5  # l distance
 
         self.liftoff_minus_state(apex_state)
@@ -889,7 +890,7 @@ class TestTakeOffPlus(unittest.TestCase):
     def liftoff_minus_state(self, apex_state):
         # Use Simulate2dHopper to simulate
         hopper, controller, state_log, animation = Simulate2dHopper(x0=apex_state,
-                                                                    duration=2,
+                                                                    duration=2.0,
                                                                     actuators_off=True)
 
         # Get simulated liftoff minus state
@@ -915,6 +916,27 @@ class TestTakeOffPlus(unittest.TestCase):
         extra_left_padding = simulated_state_index_at_touchdown_plus
         extra_right_padding = simulated_state_index_at_liftoff_minus - \
             extra_left_padding - np.shape(calculated_state_logs)[1]
+
+        print('STARING')
+        print(simulated_state_index_at_touchdown_plus)
+        print(simulated_state_index_at_liftoff_minus)
+
+        if extra_right_padding < 0:
+            extra_right_padding = 0
+            print('STARING1')
+        #     print(extra_right_padding)
+        #     print(np.shape(calculated_state_logs))
+        #     calculated_state_logs = calculated_state_logs[:,
+        #                                                   0:extra_right_padding]
+        #     print(np.shape(calculated_state_logs))
+        #     print(np.shape(simulated_state_logs))
+        #     # print(sample_times[0])
+        #     # print(sample_times[1])
+        #     # simulated_state_logs = np.pad(
+        #     #     simulated_state_logs, ((0, 0), (0, -extra_right_padding)))
+        #     # extra_right_padding = 0
+
+        print('TRYING')
 
         calculated_state_logs = np.pad(
             calculated_state_logs, ((0, 0), (extra_left_padding, extra_right_padding)))
@@ -1056,6 +1078,18 @@ class TestTakeOffPlus(unittest.TestCase):
         calculated_state_log
     ):
 
+        if np.shape(simulated_state_log)[1] != np.shape(calculated_state_log)[1]:
+            timestep = 0.0005
+            final_time = np.shape(calculated_state_log)[1] * timestep
+            print('final_time')
+            print(final_time)
+            calculated_sample_times = np.arange(0, final_time, timestep)
+            self.single_log_state_log(
+                calculated_sample_times, calculated_state_log, 'calculated_')
+            self.single_log_state_log(
+                simulated_sample_times, simulated_state_log, 'simulated_')
+            return
+
         def log_single_state(index, name):
             plt.figure().set_size_inches(10, 5)
             plt.plot(simulated_sample_times, simulated_state_log[index, :])
@@ -1085,6 +1119,35 @@ class TestTakeOffPlus(unittest.TestCase):
                     "calculated_beta", "calculated_beta_d"])
 
         log_single_state(4, 'leg_extension')
+
+        plt.show()
+
+    def single_log_state_log(
+        self,
+        sample_times,
+        state_log,
+        info
+    ):
+
+        def log_single_state(index, name):
+            plt.figure().set_size_inches(10, 5)
+            plt.plot(sample_times, state_log[index, :])
+            plt.plot(sample_times, state_log[index+5, :])
+            plt.grid(True)
+            plt.legend([name, name + "_d"])
+
+        log_single_state(0, info + 'body_x')
+        log_single_state(1, info + 'body_z')
+        log_single_state(2, info + 'body_theta')
+        log_single_state(3, info + 'alpha')
+
+        plt.figure().set_size_inches(10, 5)
+        plt.plot(sample_times, np.add(state_log[2, :], state_log[3, :]))
+        plt.plot(sample_times, np.add(state_log[2+5, :], state_log[3+5, :]))
+        plt.grid(True)
+        plt.legend([info + "beta", info + "beta_d"])
+
+        log_single_state(4, info + 'leg_extension')
 
         plt.show()
 

project/hopper_2d.py
@@ -283,6 +283,7 @@ class Hopper2dController(VectorSystem):
 
         state_logs = np.copy(current_state)
         state_logs = state_logs[:, np.newaxis]
+        print('START: {:.{}f}'.format(current_state[1+5], 3))
 
         # touchdown_time = 0.645
 
@@ -304,7 +305,7 @@ class Hopper2dController(VectorSystem):
                 l_rest = 1.0
             leg_compression_amount = l_rest - current_state[4]
             # Somehow, all the values in the simulator of mass & spring constant don't seem to be correct
-            k_used_by_simulator = 80.0
+            k_used_by_simulator = self.K_l  # 80.0
             spring_force = k_used_by_simulator * leg_compression_amount
 
             # Spring is pushing back only the body, not the foot's mass
@@ -362,8 +363,13 @@ class Hopper2dController(VectorSystem):
                 new_velocity_perpendicular_to_leg_frame * math.cos(beta)
             # On the first step, this drastically reduces and causes disprepancies.
             current_state[1+5] = new_velocity_along_leg_frame * \
-                math.cos(beta) + \
+                math.cos(beta) - \
                 new_velocity_perpendicular_to_leg_frame * math.sin(beta)
+
+            print('along: {:.{}f}'.format(new_velocity_along_leg_frame, 3) +
+                  '\t perpendicular: {:.{}f}'.format(new_velocity_perpendicular_to_leg_frame, 3) +
+                  '\t beta: {:.{}f}'.format(beta, 3))
+            print('1: {:.{}f}'.format(current_state[1+5], 3))
 
             # Set new positions
             current_state[0] = current_state[0] + current_state[0+5] * timestep
@@ -508,8 +514,6 @@ class Hopper2dController(VectorSystem):
               str(desired_liftoff_minus_speed))
         print('current_desired_touchdown_beta: \t' +
               str(self.current_desired_touchdown_beta))
-        print('self.current_desired_l_at_bottom: \t' +
-              str(self.current_desired_l_at_bottom))
 
         return self.get_beta(state[2], state[3]), l_at_bottom
 
@@ -576,6 +580,8 @@ class Hopper2dController(VectorSystem):
 
         print('Hop #' +
               str(self.total_number_of_hops))
+        print('self.current_desired_l_at_bottom: \t' +
+              str(self.current_desired_l_at_bottom))
 
         current_beta = self.get_beta(current_state[2], current_state[3])
         current_betad = self.get_beta(current_state[2+5], current_state[3+5])